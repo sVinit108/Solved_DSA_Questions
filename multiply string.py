50bf5008cfde4c6a4306fdfe3d229a2a3a2a4895
Vinit <sharmavinit.1008@gmail.com>
2023-07-09 13:19:28 +0530

    Multiply Strings

---
+++ multiply string.py
@@ -0,0 +1,6 @@
+#Link:- https://leetcode.com/problems/multiply-strings/submissions/989988190/
+
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        ans=int(num1)*int(num2)
+        return f"{ans}"
